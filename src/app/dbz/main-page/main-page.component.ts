1f1b88ad28caf55d9ae7c5fcd4c8ef6227630045
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-06-24 14:07:31 -0400

    Mostrar listado personajes

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -10,12 +10,34 @@ interface ICharacter {
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
+  // arrays of characters
+  characters: ICharacter[] = [
+    {
+      name: 'Goku',
+      power: 15000,
+    },
+    {
+      name: 'Vegeta',
+      power: 7500,
+    },
+  ];
+
   newCharacter: ICharacter = {
-    name: 'Trucks',
-    power: 14000,
+    name: '',
+    power: 0,
   };
 
   addSubmit() {
+    if (this.newCharacter.name.trim().length === 0) {
+      return;
+    }
     console.log(this.newCharacter);
+
+    //TODO: add characters
+    this.characters.push(this.newCharacter);
+    this.newCharacter = {
+      name: '',
+      power: 0,
+    };
   }
 }
