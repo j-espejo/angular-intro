d636bcf1b095b8c408e929010d405d0f74b4ed21
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-06-24 12:54:11 -0400

    ngModel

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 
+interface ICharacter {
+  name: string;
+  power: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
+  newCharacter: ICharacter = {
+    name: 'Trucks',
+    power: 14000,
+  };
+
   addSubmit() {
-    console.log('submit');
+    console.log(this.newCharacter);
   }
 }
